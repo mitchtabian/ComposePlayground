d21fd926dd02517c084328e84d3d4f11621e2bf8
mitch <mitch@tabian.ca>
2020-10-30 18:25:44 -0700

    saving state and capturing input

--- app/src/main/java/com/codingwithmitch/composeplayground/ui/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/composeplayground/ui/MainActivity.kt
@@ -2,25 +2,24 @@ package com.codingwithmitch.composeplayground.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.viewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.ui.tooling.preview.Preview
-import com.codingwithmitch.composeplayground.R
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+
 
 class MainActivity : AppCompatActivity(){
 
@@ -28,58 +27,49 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
 
         setContent {
-            Profile ()
+            InputCaptureScreen ()
         }
 
     }
 }
 
+class InputCaptureViewModel: ViewModel() {
+
+    private val _name = MutableLiveData("")
+    val name: LiveData<String> = _name
+
+    fun onNameChanged(newName: String) {
+        _name.value = newName
+    }
+}
+
 @Composable
-fun Profile() {
-    val image = imageResource(id = R.drawable.resource1)
+fun InputCaptureScreen(
+    viewModel: InputCaptureViewModel = viewModel()
+) {
+
+    // Requires: import androidx.compose.runtime.getValue
+    val name: String by viewModel.name.observeAsState("")
+
     MaterialTheme{
-        ScrollableColumn (
-            modifier = Modifier.padding(16.dp)
-        ){
-            val imageModifier = Modifier
-                .preferredHeight(180.dp)
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
                 .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(4.dp))
+        ) {
 
-            Image(
-                image,
-                modifier = imageModifier,
-                contentScale = ContentScale.Crop
+            TextField(
+                value = name,
+                onValueChange = {viewModel.onNameChanged(it)},
+                label = { Text("Name")},
+                modifier = Modifier.fillMaxWidth()
             )
-
-            val columnModifer = Modifier
-                .fillMaxHeight()
-            Column(
-                modifier = columnModifer,
-                verticalArrangement = Arrangement.Top
-            ) {
-                Text(
-                    "Mitch Tabian",
-                    style = typography.h6,
-                    modifier = Modifier
-                        .padding(top = 8.dp,)
-                )
-                Text(
-                    "Abbotsford, BC, Canada",
-                    style = typography.subtitle1,
-                    maxLines = 1,
-                    fontSize = 12.sp,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    stringResource(id = R.string.example_body),
-                    style = typography.body1,
-                    fontSize = 15.sp,
-                    modifier = Modifier
-                        .padding(top = 10.dp,)
-
-                )
-            }
+            Text(
+                name,
+                style = typography.h3,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+            )
         }
     }
 }
@@ -88,7 +78,7 @@ fun Profile() {
 @Preview
 @Composable
 fun Preview() {
-    Profile()
+    InputCaptureScreen()
 }
 
 
