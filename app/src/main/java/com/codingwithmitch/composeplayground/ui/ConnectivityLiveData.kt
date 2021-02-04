5b16093e070a349053a9d989f72ff646abe229ca
mitch <mitch@tabian.ca>
2021-02-04 11:52:35 -0800

    monitor network connectivity using LiveData.

---
+++ app/src/main/java/com/codingwithmitch/composeplayground/ui/ConnectivityLiveData.kt
@@ -0,0 +1,72 @@
+package com.codingwithmitch.composeplayground.ui
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest
+import android.util.Log
+import androidx.lifecycle.LiveData
+
+/**
+ * Save all available networks with an internet connection to a set (@validNetworks).
+ * As long as the size of the set > 0, this LiveData emits true.
+ * MinSdk = 21.
+ *
+ * Inspired by:
+ * https://github.com/AlexSheva-mason/Rick-Morty-Database/blob/master/app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/networking/ConnectionLiveData.kt
+ */
+class ConnectionLiveData(context: Context) : LiveData<Boolean>() {
+
+    private val TAG = "C-Manager"
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val validNetworks: MutableSet<Network> = HashSet()
+
+    private fun checkValidNetworks(){
+        postValue(validNetworks.size > 0)
+    }
+
+    override fun onActive() {
+        networkCallback = createNetworkCallback()
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NET_CAPABILITY_INTERNET)
+            .build()
+        cm.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onInactive() {
+        cm.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun createNetworkCallback() = object : ConnectivityManager.NetworkCallback() {
+
+        /*
+          Called when a network is detected. If that network has internet, save it in the Set.
+          Source: https://developer.android.com/reference/android/net/ConnectivityManager.NetworkCallback#onAvailable(android.net.Network)
+         */
+        override fun onAvailable(network: Network) {
+            Log.d(TAG, "onAvailable: ${network}")
+            val networkCapabilities = cm.getNetworkCapabilities(network)
+            val isInternet = networkCapabilities?.hasCapability(NET_CAPABILITY_INTERNET)
+            Log.d(TAG, "onAvailable: ${network}, $isInternet")
+            if(isInternet ==  true){
+                validNetworks.add(network)
+            }
+            checkValidNetworks()
+        }
+
+        /*
+          If the callback was registered with registerNetworkCallback() it will be called for each network which no longer satisfies the criteria of the callback.
+          Source: https://developer.android.com/reference/android/net/ConnectivityManager.NetworkCallback#onLost(android.net.Network)
+         */
+        override fun onLost(network: Network) {
+            Log.d(TAG, "onLost: ${network}")
+            validNetworks.remove(network)
+            checkValidNetworks()
+        }
+
+    }
+
+}
--- app/src/main/java/com/codingwithmitch/composeplayground/ui/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/composeplayground/ui/MainActivity.kt
@@ -2,56 +2,69 @@ package com.codingwithmitch.composeplayground.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var connectionLiveData: ConnectionLiveData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        connectionLiveData = ConnectionLiveData(this)
         setContent {
-            val query = remember { mutableStateOf("") }
+            val isNetworkAvailable = connectionLiveData.observeAsState(initial = false).value
+            ConnectivityMonitor(isNetworkAvailable = isNetworkAvailable)
+        }
+    }
+}
 
-            Column(modifier = Modifier.fillMaxWidth()) {
-                TextField(
-                        modifier = Modifier.padding(16.dp),
-                        value = query.value,
-                        onValueChange = {
-                            query.value = it
-                        },
-                        keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Text,
-                                imeAction = ImeAction.Search,
-                        ),
-                        onImeActionPerformed = { action, softKeyboardController ->
-                            if (action == ImeAction.Search) {
-                                softKeyboardController?.hideSoftwareKeyboard()
-                            }
-                        },
+@Composable
+fun ConnectivityMonitor(
+    isNetworkAvailable: Boolean,
+){
+    var connectionString = "Valid connection."
+    if(!isNetworkAvailable){
+        connectionString = "No network connection"
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter)){
+            Row(modifier = Modifier.align(Alignment.CenterHorizontally)){
+                Text(
+                    connectionString,
+                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.h6
                 )
-                Button(
-                        onClick = { query.value = "Chicken" },
-                        modifier = Modifier.padding(16.dp)
-                ) {
-                    Text("Trigger issue")
+                if(isNetworkAvailable){
+                    Icon(
+                        Icons.Default.Check,
+                        modifier = Modifier.padding(8.dp),
+                        contentDescription = "Checkmark icon"
+                    )
+                }
+                else{
+                    Icon(
+                        Icons.Default.Close,
+                        modifier = Modifier.padding(8.dp),
+                        contentDescription = "X icon"
+                    )
                 }
             }
         }
-
     }
+
 }
 
 
